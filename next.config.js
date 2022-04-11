d6af7de19947a424994907943ed144dc58160c78
muzman01 <supurge_man@hotmail.com>
2022-04-11 15:33:22 +0300

    tettttt

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    BASE_URL: "http://localhost:3000",
+    BASE_URL: "https://mmsellvote.vercel.app",
     MONGODB_URL:"mongodb+srv://blokfieldsellvote:Gerok261.@cluster0.xqejn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     ACC_KEY:"",
     ADMİN:"muzman",
