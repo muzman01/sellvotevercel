2eaf4a08b337db30182d47a4f7cce543a7b00c8d
muzman01 <supurge_manq@hotmail.com>
2022-06-03 15:05:27 +0300

    url güncelleme

--- store/Globalstate.js
+++ store/Globalstate.js
@@ -2,7 +2,7 @@ import { createContext, useEffect, useReducer } from "react";
 import reducers from "./Reducers";
 import axios from "axios";
 export const DataContext = createContext();
-
+const baseUrl = process.env.BASE_URL
 export const DataProvider = ({ children }) => {
   const initialState = {
     notify: {},
@@ -17,7 +17,7 @@ export const DataProvider = ({ children }) => {
   const { votepowerR, votweigthR, steemdlrR, sdbdlrR, busddlrR } = state;
   useEffect(() => {
     try {
-      axios.get(`http://localhost:3000/api/calculation`).then((data) => {
+      axios.get(`${baseUrl}/api/calculation`).then((data) => {
         dispatch({ type: "VOTEPOWER", payload: data.data.lastValue });
         dispatch({ type: "VOTEWEİGTH", payload: data.data.powerw });
      
