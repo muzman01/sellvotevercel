31b6aa373255fd6b84559d65a1e0931ddf7a2f5c
muzman01 <supurge_man@hotmail.com>
2022-07-23 13:22:24 +0300

    aa

--- pages/api/calculation/index.js
+++ pages/api/calculation/index.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
     async function startBot() {
       // let lastValue = getAccountDetailDolar();
       // let powerw = getAccountDetailPower();
-      let lastValue = 6.17;
+      let lastValue = 31.31;
       let powerw = 90.24;
       res.status(200).json({
         lastValue,
